e4e790ea5fcf9f50ee122a4705d7c42c3eaad96c
Jose <josemsg@gmail.com>
2018-07-07 15:06:18 +0200

    Añadida tabla para la base de datos

---
+++ admin/sql/install.mysql.utf8.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `#__frecuenciarespiratoria` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `resultado` varchar(255) NOT NULL DEFAULT '',
+    `estado` tinyint(1) NOT NULL default '0',
+    `fecha_creacion` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
